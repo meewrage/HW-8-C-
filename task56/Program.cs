7af2ca977b783f8c6d70d2ea360115688ea1f421
meewrage <luki_wop@mail.ru>
2023-03-13 22:01:52 +0300

    added task56

---
+++ task56/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+int ReadNumber(string messageToUser)     //метод считывания целых чисел
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int[,] GetRandomMatrix(int rows, int colums, int leftBorder = 0, int rightBorder = 10) // создает матрицу
+{
+ 
+    int[,] matrix = new int[rows, colums];
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.Next(leftBorder, rightBorder +1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)  // выводит матрицу на консоль
+{
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+void Sum(int[,] matrix)
+{
+    int index = 0;
+    
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        int minsum = 0;
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+                    
+            
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        line += matrix[index, j] + " ";
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {line}. ");
+}
+        
+
+int m = ReadNumber("Введите кол-во строк и столбцов");
+int n = m;
+int[,] myMatrix = GetRandomMatrix(m, n);
+PrintMatrix(myMatrix);
+Console.WriteLine();
+Sum(myMatrix);
